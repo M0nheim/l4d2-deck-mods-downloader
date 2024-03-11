11ef6f19a0963734027fde139c0e494800739d41
M0nheim <simon.henties@gmail.com>
2024-03-11 15:02:24 +0100

    add mario2.vpk

--- download.sh
+++ download.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -O https://github.com/M0nheim/l4d2-deck-mods-downloader/blob/main/maps/mario2.vpk
+mv /home/deck/Desktop/mario2.vpk "/home/deck/.local/share/Steam/steamapps/common/Left 4 Dead 2/left4dead2/addons/"
