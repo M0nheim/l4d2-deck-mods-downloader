745e0b6a723d3c71b64e3716ab62c74290194c7b
M0nheim <simon.henties@gmail.com>
2024-03-11 15:13:48 +0100

    lol

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@
 echo "Downloading Map"
 curl -O https://github.com/M0nheim/l4d2-deck-mods-downloader/blob/main/maps/mario2.vpk
 echo "Download finished"
-mv /home/deck/Desktop/mario2.vpk "/home/deck/.local/share/Steam/steamapps/common/Left 4 Dead 2/left4dead2/addons/"
+mv /home/deck/mario2.vpk "/home/deck/.local/share/Steam/steamapps/common/Left 4 Dead 2/left4dead2/addons/"
 $BASH
