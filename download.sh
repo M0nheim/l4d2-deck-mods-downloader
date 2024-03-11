38db6ceaac4041dd92cb86b3a8a1400d7faa5f89
M0nheim <simon.henties@gmail.com>
2024-03-11 15:16:53 +0100

    possibly making it work the first time

--- download.sh
+++ download.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "Downloading Map"
+cd "/home/deck/.local/share/Steam/steamapps/common/Left 4 Dead 2/left4dead2/addons/"
 curl -O https://github.com/M0nheim/l4d2-deck-mods-downloader/blob/main/maps/mario2.vpk
-echo "Download finished"
-mv /home/deck/mario2.vpk "/home/deck/.local/share/Steam/steamapps/common/Left 4 Dead 2/left4dead2/addons/"
-$BASH
