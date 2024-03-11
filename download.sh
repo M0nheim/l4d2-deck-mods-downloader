b897816ae86e9125722438a515198d573720f6a7
M0nheim <simon.henties@gmail.com>
2024-03-11 15:59:57 +0100

    fix raw link to map

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "/home/deck/.local/share/Steam/steamapps/common/Left 4 Dead 2/left4dead2/addons/"
-curl -O https://github.com/M0nheim/l4d2-deck-mods-downloader/blob/main/maps/mario2.vpk
+curl -O https://github.com/M0nheim/l4d2-deck-mods-downloader/raw/main/maps/mario2.vpk
